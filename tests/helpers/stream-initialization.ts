235564938406a45c8286db956d457816ab1212f2
Mark Stacey <markjstacey@gmail.com>
2023-02-17 17:23:04 -0330

    Add comments to `setupStreamInitialization`

--- tests/helpers/stream-initialization.ts
+++ tests/helpers/stream-initialization.ts
@@ -13,6 +13,9 @@ declare global {
  *
  * The returned array will continue to grow over time as new logs are added.
  *
+ * This is based on the "Mock Browser APIs" example in the Playwright documentation.
+ *
+ * @see {@link https://playwright.dev/docs/mock-browser-apis#verifying-api-calls}
  * @param page - The Playwright page object.
  * @returns An array of `postMessage` logs, excluding initialization.
  */
@@ -21,21 +24,26 @@ export async function setupStreamInitialization(
 ): Promise<{ message: string; targetOrigin: string }[]> {
   const logs: { message: string; targetOrigin: string }[] = [];
 
+  // This attaches a `logCall` function to `window` that will send a message to
+  // Playwright to execute this function in the test runner Node.js process.
   await page.exposeFunction(
     'logCall',
     (message: string, targetOrigin: string) =>
       logs.push({ message, targetOrigin }),
   );
 
+  // This function is executed in the browser, in the page under test
   await page.addInitScript(() => {
     window.originalPostMessage = window.postMessage.bind(window);
     window.postMessage = (message, targetOrigin) => {
       if (message?.target === 'metamask-contentscript') {
         if (message.data === 'SYN') {
+          // The extension replies with "ACK" as part of establishing a connection
           window.originalPostMessage(
             { target: 'metamask-phishing-warning-page', data: 'ACK' },
             targetOrigin,
           );
+          // The extension replies to "ACK" with another "ACK", which we can ignore
         } else if (message.data !== 'ACK') {
           window.logCall(message.data, targetOrigin);
         }
